cafae91f5ec7506abeee400c4e4292f5b2cb03c8
Andreas Amstutz <andreasamstutz@gmail.com>
2016-01-16 17:46:54 +0100

    Just a commit to trigger travis

--- test/hello.js
+++ test/hello.js
@@ -2,4 +2,14 @@ describe("hello", function () {
     it("should work", function () {
         expect(true).toBe(true);
     })
+
+    var jasmine = getJasmineRequireObj().core(jasmineRequire);
+    env = jasmine.getEnv();
+
+    env.describe('one', function(){
+        env.describe('nested', function(){
+            env.it('should do something', function(){});
+        });
+    });
+
 });
