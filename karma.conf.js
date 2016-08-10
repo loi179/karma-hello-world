6cb2d9e47c26c3c09081dd68d66f4a74322f2170
loibh <loi.bui@orientsoftware.net>
2016-08-10 09:50:24 +0700

    phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Sat Jan 16 2016 14:58:08 GMT+0100 (CET)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-	"test/hello.js"
+      "test/hello.js"
     ],
 
 
@@ -55,15 +55,20 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    plugins: [
+      "karma-jasmine",
+      "karma-phantomjs-launcher",
+    ]
   })
 }
